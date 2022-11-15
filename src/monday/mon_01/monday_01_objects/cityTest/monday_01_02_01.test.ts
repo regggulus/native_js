9f70fbbff3261dc6e34ad77369ef1554e2a56b3c
Egorych <mikhalevichegor@gmail.com>
2022-11-15 18:31:37 +0300

    Test/monday01_02_01 object new components

---
+++ src/monday/mon_01/monday_01_objects/cityTest/monday_01_02_01.test.ts
@@ -0,0 +1,14 @@
+import {CityType} from "./monday_01_02_01";
+
+let city: CityType;
+
+beforeEach( () => {
+    city = {
+        title: "New York",
+        houses: [],
+        governmentBuilding: [],
+        citizensNumber: 100000
+    }
+})
+//01. создайте тип CityType
+//02. заполните объект city, чтобы тесты ниже прошли
---
+++ src/monday/mon_01/monday_01_objects/cityTest/monday_01_02_01.ts
@@ -0,0 +1,7 @@
+export const value3 = "monday_02_01"
+export type CityType = {
+    title: string
+    houses: Array<string>
+    governmentBuilding: Array<string>
+    citizensNumber: number
+}
