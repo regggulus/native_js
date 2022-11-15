c386206f1d2609921d2832bce8024f0b1fb69fb8
Egorych <mikhalevichegor@gmail.com>
2022-11-15 16:48:47 +0300

    Test/monday_01

--- src/monday/mon_01/monday_01_tests/monday01_01.js
+++
@@ -1,6 +0,0 @@
-export const value = "monday_01"
-
-const sentence = "Hello my Friends!"
-
-
-
---
+++ src/monday/mon_01/monday_01_tests/monday01_01.test.ts
@@ -0,0 +1,52 @@
+import {mult, splitIntoWords, sum} from "./monday01_01";
+
+let a: number
+let b: number
+let c: number
+
+beforeEach(() => {
+    a = 1
+    b = 2
+    c = 3
+})
+test("amount must be correct", () => {
+
+    let result1 = sum(a, b)
+    let result2 = sum(b, c)
+
+    expect(result1).toBe(3)
+    expect(result2).toBe(5)
+
+})
+test("multiply should be correct", () => {
+
+    let a = 1
+    let b = 2
+    let c = 3
+
+    let result1 = mult(a, c)
+    let result2 = mult(b, c)
+
+    expect(result1).toBe(3)
+    expect(result2).toBe(6)
+})
+
+test("splitting into should be correct", () => {
+    let sent1 = "hello my friend"
+    let sent2 = "Js the best programming language"
+
+    let result1 = splitIntoWords(sent1)
+    let result2 = splitIntoWords(sent2)
+
+    expect(result1.length).toBe(3)
+    expect(result1[0]).toBe("hello")
+    expect(result1[1]).toBe("my")
+    expect(result1[2]).toBe("friend")
+
+    expect(result2.length).toBe(5)
+    expect(result2[0]).toBe("js")
+    expect(result2[1]).toBe("the")
+    expect(result2[2]).toBe("best")
+    expect(result2[3]).toBe("programming")
+    expect(result2[4]).toBe("language")
+})
