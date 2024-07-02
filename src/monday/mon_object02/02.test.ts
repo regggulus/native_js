39416cb725755592ce1a432a6e8935f691767dfb
Egorych <mikhalevichegor@gmail.com>
2024-07-03 01:05:20 +0300

    test CityType

---
+++ src/monday/mon_object02/02.test.ts
@@ -0,0 +1,17 @@
+let city: CityType;
+
+beforeEach(() => {
+    city = {
+        title: 'New York',
+        houses: [],
+        governmetBuilding: [],
+        citizensNumber: 100000
+    }
+})
+
+test('test city should contains 3 houses', () => {
+
+})
+test('test city should contains hospital and station', () => {
+
+})
