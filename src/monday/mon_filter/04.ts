d3bcd41998e0ef2d4c563c3ca7d1b7c81a7535bc
Egorych <mikhalevichegor@gmail.com>
2024-07-22 23:47:11 +0300

    component MonFilter.ts

---
+++ src/monday/mon_filter/04.ts
@@ -0,0 +1,30 @@
+export const age = [10, 24, 36, 48, 55, 63, 78, 88, 96, 101]
+
+const predicate = (age: number) => {
+    return (
+        age > 37
+    )
+}
+
+const oldAgest = [63]
+
+type CourseType = {
+    title: string
+    price: number
+}
+
+const course = [
+    {title: 'CSS', price: 120},
+    {title: 'JavaScript', price: 220},
+    {title: 'React', price: 150},
+    {title: 'HTML', price: 50},
+]
+
+const chipPredicate = (course: CourseType) => {
+    return course.price < 130
+}
+
+const chipCourse = [
+    {title: 'CSS', price: 110},
+    {title: 'REACT', price: 130}
+]
