eee5a18224024799421d67d612306efbbd761718
Egorych <mikhalevichegor@gmail.com>
2024-07-25 00:41:52 +0300

    test filter old

--- src/monday/mon_filter/04.test.ts
+++ src/monday/mon_filter/04.test.ts
@@ -1,7 +1,12 @@
 
 test('should be tale old', ()=> {
-    const ages = [10, 24, 36, 48, 55, 63, 78, 88, 96, 101]
-    const oldAges = ages
+    const age = [10, 24, 36, 48, 55, 63, 78, 88, 96, 101]
+    const predicat = (age: number) => {
+        return age > 48
+    }
+    const oldAges = age.filter(predicat)
 
-    expect(oldAges.length).toBe(1);
+    expect(oldAges.length).toBe(6);
+    expect(oldAges[0]).toBe(55)
 })
+export {}
