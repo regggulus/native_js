69adb29d5ff9a37b768ecfd16d484fe7b43dd04b
Egorych <mikhalevichegor@gmail.com>
2024-12-22 22:49:44 +0300

    object, function, tests

--- src/monday/mon_test01/01.test.ts
+++ src/monday/mon_test01/01.test.ts
@@ -1,26 +1,31 @@
-import {mult, splitIntoWords, sum} from "./01";
+import {mult, splitIntoWords, sum, division} from "./01";
 
+let a: number
+let b: number
+let c: number
+beforeEach(() => {
+    a = 6
+    b = 8
+    c = 3
+})
 test('sum should be correct', () => {
-    const a = 1
-    const b = 2
-    const c = 3
+
 
     const result1 = sum(a, b)
     const result2 = sum(b, c)
+    const result3 = sum(a, c)
 
     expect(result1).toBe(3)
     expect(result2).toBe(5)
+    expect(result3).toBe(2)
 })
 test('multiply should be correct', () => {
-    const a = 1
-    const b = 2
-    const c = 3
 
     const result1 = mult(a, b)
     const result2 = mult(b, c)
 
-    expect(result1).toBe(2)
-    expect(result2).toBe(6)
+    expect(result1).toBe(48)
+    expect(result2).toBe(24)
 })
 test('splitting into words should be correct', () => {
     const sent1 = 'Hello my friends'
--- src/monday/mon_test01/01.ts
+++ src/monday/mon_test01/01.ts
@@ -5,6 +5,9 @@ export function sum(a: number, b: number) {
 export function mult(a: number, b: number) {
     return a * b
 }
+export function division(a: number, c: number) {
+return a / c
+}
 export function splitIntoWords(sentense: string) {
     return sentense.split(' ')
 }
